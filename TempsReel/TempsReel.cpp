f3b2d5d0b62afcb2a57f994c7f07cec6ce8f2810
LilTsubaki <omenforce.clem@neuf.fr>
2016-01-07 16:36:26 +0100

    shadow map okok

--- TempsReel/TempsReel.cpp
+++ TempsReel/TempsReel.cpp
@@ -191,10 +191,11 @@ static float cpt2 = 0;
 static float cpt3 = 0;
 
 static float tx = -0.5;
-static float datas[] = {-0.5,-0.5,0, 0.5,-0.5,0, 0.5,0.5,0, -0.5,0.5,0};
+//static float datas[] = {-0.5,-0.5,0, 0.5,-0.5,0, 0.5,0.5,0, -0.5,0.5,0};
 static GLuint buffer; 
 static glm::mat4 projectionMatrix;
 static glm::mat4 viewMatrix;
+static glm::mat4 luxMatrix;
 static int cptCamera = 0;
 static int nbTriangles;
 
@@ -203,17 +204,34 @@ struct
 {
 	GLuint program; // a shader
 	GLuint vao; // a vertex array object
+	GLuint depthTexture;
+	GLuint fbo;
 } gs;
 
 
 void init()
 {
+	glGenTextures(1, &gs.depthTexture);
+	glBindTexture(GL_TEXTURE_2D, gs.depthTexture);
+	glTexStorage2D(GL_TEXTURE_2D, 1, GL_DEPTH_COMPONENT32F, 800, 800);
+
+	glGenFramebuffers(1, &gs.fbo);
+	glBindFramebuffer(GL_FRAMEBUFFER, gs.fbo);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, gs.depthTexture, 0);
+
+	assert(glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, gs.depthTexture);
+
 	// Build our program and an empty VAO
 	gs.program = buildProgram("basic.vsl", "basic.fsl");
 
 	Maillage m;
 	glm::vec3 min;
 	glm::vec3 max;
+
 	m.geometry(glm::vec3(0.f, 0.f, 0.f), "C:/Users/etu/Desktop/monkey.obj", min, max);
 
 	std::vector<float> points = m.getAllPoints();
@@ -256,11 +274,12 @@ void init()
 	
 }
 
-void render(GLFWwindow* window)
+
+void pass(GLuint buffer, int width, int height, glm::mat4 viewMatrix)
 {
-	int width, height;
-	glfwGetFramebufferSize(window, &width, &height);
+	glBindFramebuffer(GL_FRAMEBUFFER, buffer);
 	glViewport(0, 0, width, height);
+
 	glClear(GL_COLOR_BUFFER_BIT);
 	glClear(GL_DEPTH_BUFFER_BIT);
 
@@ -269,38 +288,50 @@ void render(GLFWwindow* window)
 	glEnable(GL_DEPTH_TEST);
 
 	projectionMatrix = glm::perspective(45.0f, 640.0f / 480.0f, 1.0f, 200.0f);
-	viewMatrix = glm::lookAt(glm::vec3(sin(cptCamera/100.0), cos(cptCamera / 100.0), -3), glm::vec3(0, 0, 0), glm::vec3(0, 0, -1));
 
 	glm::mat4 MVP = projectionMatrix * viewMatrix;
 
+
 	glProgramUniformMatrix4fv(gs.program, 15, 1, GL_FALSE, &MVP[0][0]);
+
 
 	glProgramUniform1f(gs.program, 3, 1); //fmod(cpt1 += 0.01, 1));
 	glProgramUniform1f(gs.program, 4, 1); //fmod(cpt2 += 0.05, 1));
 	glProgramUniform1f(gs.program, 5, 1); // fmod(cpt3 += 0.02, 1));
+
+	glProgramUniform3f(gs.program, 6, 0.5, -1, -2);
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	glDrawArrays(GL_TRIANGLES, 0, nbTriangles);
-	/*glProgramUniform1f(gs.program, 3, fmod(cpt1 += 0.01, 1));
-	glProgramUniform1f(gs.program, 4, fmod(cpt2 += 0.05, 1));
-	glProgramUniform1f(gs.program, 5, fmod(cpt3 += 0.02, 1));
 
-	glProgramUniform1f(gs.program, 6, (sin(tx++/100.0)+1)*0.75-1);
-	glProgramUniform1f(gs.program, 7, (sin(tx++ / 100.0) + 1)*0.75 - 1);
-		
+	glViewport(0, 0, width, height);
+
+	glUseProgram(0);
+}
 
 
-	glProgramUniform1f(gs.program, 3, fmod(cpt1 + 0.5, 1));
-	glProgramUniform1f(gs.program, 4, fmod(cpt2 + 0.01, 1));
-	glProgramUniform1f(gs.program, 5, fmod(cpt3 + 0.01, 1));
+void render(GLFWwindow* window)
+{
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
 
-	glProgramUniform1f(gs.program, 6, (sin(tx++ / 80.0) + 1)*0.75 - 1);
-	glProgramUniform1f(gs.program, 7, (cos(tx++ / 100) + 1)*0.75 - 1);
+	luxMatrix = glm::lookAt(glm::vec3(0.5, -1, -2), glm::vec3(0, 0, 0), glm::vec3(0, -1, 0));
+	viewMatrix = glm::lookAt(glm::vec3(sin(cptCamera++ / 100.0) * 5, -5, cos(cptCamera / 100.0) * 5), glm::vec3(0, 0, 0), glm::vec3(0, -1, 0));
 
-	glDrawArrays(GL_TRIANGLE_FAN, 0, 4);*/
+	projectionMatrix = glm::perspective(45.0f, 640.0f / 480.0f, 1.0f, 200.0f);
+	glm::mat4 MVP = projectionMatrix * luxMatrix;
+	glProgramUniformMatrix4fv(gs.program, 16, 1, GL_FALSE, &MVP[0][0]);
+
+
+
+	
 	
 
-	glBindVertexArray(0);
-	glUseProgram(0);
+	pass(gs.depthTexture, 800, 800, luxMatrix);
+
+	pass(0, width, height, viewMatrix);
+
+
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
 
